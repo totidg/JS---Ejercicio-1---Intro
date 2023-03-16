3a77429627669d7f32568f279f9d4bdf565d0cde
totidg <113801644+totidg@users.noreply.github.com>
2023-03-15 22:53:32 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# JS---Ejercicio-1---Intro
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-gwe7kp)
---
+++ script.js
@@ -0,0 +1,11 @@
+// Uso de JS Externo
+document.write('JS - Ejercicio 1');
+
+// Comentarios cortos de una sola línea. Suelen explicar la línea siguiente.
+var a = 1;
+
+var x = 45; // También se utilizan al final de una línea.
+
+/* Por otro lado, existen los comentarios múltiples de varias líneas consecutivas.
+   Suelen utilizarse para explicaciones largas que requieren bastante
+   espacio porque se mencionan gran cantidad de cosas :-) */
